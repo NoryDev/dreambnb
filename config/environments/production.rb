9f0ffa59a51fd39739a837b47924b69d9f82af5b
Fabien Loup <info@norydev.com>
2016-06-20 13:50:04 +0300

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => "dreambnb.herokuapp.com",
+    :authentication => :plain,
   }
 
-  ActionMailer::Base.delivery_method = :smtp
-
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'http://dreambnb.herokuapp.com' }
 
   # Code is not reloaded between requests.
